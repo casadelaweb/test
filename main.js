6f35dc505fc566639343d957c606956302138b4d
Alex Volkov <a.volkov@3l.ru>
2023-08-31 11:22:11 +0500

    фикс

--- main.js
+++ main.js
@@ -22,16 +22,18 @@ class Timer {
     }
     this.remainingTime = this.options.duration
     this.elements = this.updateElements()
+    this.onClick = this.handleClick.bind(this)
   }
 
   updateElements() {
     const { body } = document
 
     const timerElement = body.querySelector(this.selectors.timer)
-    const timerMessage = body.querySelector(this.selectors.message)
+    const timerMessage = timerElement.querySelector(this.selectors.message)
+    const timerButton = timerElement.querySelector(this.selectors.button)
 
     return {
-      timerElement, timerMessage
+      timerElement, timerMessage, timerButton
     }
   }
 
@@ -49,19 +51,25 @@ class Timer {
 
   init() {
     this.elements = this.updateElements()
-    this.elements.timerMessage.textContent = ''
     this.setDefaultState()
+    this.updateListeners()
+  }
 
-    document.addEventListener('click', (event) => {
-      const { target } = event
-      if (target.closest(this.selectors.button)) this.startTimer()
-    })
+  updateListeners() {
+    document.removeEventListener('click', this.onClick)
+    document.addEventListener('click', this.onClick)
+  }
+
+  handleClick(event) {
+    const { target } = event
+    if (target.closest(this.selectors.button)) this.startTimer()
   }
 
   setDefaultState() {
     this.isStopped = true
     this.remainingTime = this.options.duration
     this.elements.timerMessage.textContent = 'Можно отправить повторно'
+    this.elements.timerButton.classList.remove('disabled')
     clearInterval(this.timer)
   }
 
@@ -69,6 +77,7 @@ class Timer {
     if (this.isStopped) {
       this.isStopped = false
       this.elements.timerMessage.textContent = `До повторной отправки ${ this.options.duration } секунд`
+      this.elements.timerButton.classList.add('disabled')
       this.timer = setInterval(this.renderTime.bind(this), this.options.step)
     }
   }
